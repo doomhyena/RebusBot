dc832300344cebccf3d5e307f94b6240972bd4f6
Thomas <thomashenderson305@gmail.com>
2023-06-19 14:00:01 +0200

    Add project files.

---
+++ DiscordBot/Bot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace DiscordBot
+{
+    class Bot
+    {
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
